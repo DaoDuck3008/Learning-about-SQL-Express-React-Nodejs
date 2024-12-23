a166205da6d997fb4abad6ad09f39adadf529978
Dao Duck <dduccraft@gmail.com>
2024-12-23 10:56:18 +0700

    change create service to create ORM

--- src/service/CRUDservice.js
+++ src/service/CRUDservice.js
@@ -1,6 +1,6 @@
 import pool from "../config/database";
 import bcrypt from "bcryptjs";
-
+import db from "../models/models/index";
 const salt = bcrypt.genSaltSync(10);
 
 const hashUserPassword = (userPassword) => {
@@ -10,11 +10,21 @@ const hashUserPassword = (userPassword) => {
 
 const createUser = async (email, password, username) => {
   const hashPass = hashUserPassword(password);
-  const [results, fields] = await pool.query(
-    `INSERT INTO user (email, password, username) VALUES (?,?,?)`,
-    [email, hashPass, username]
-  );
-  return results;
+  // const [results, fields] = await pool.query(
+  //   `INSERT INTO user (email, password, username) VALUES (?,?,?)`,
+  //   [email, hashPass, username]
+  // );
+  // return results;
+
+  try {
+    await db.User.create({
+      username: username,
+      email: email,
+      password: hashPass,
+    });
+  } catch (error) {
+    console.log(">>> error: ", error);
+  }
 };
 
 const deleteUser = async (id) => {
